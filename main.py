4cc94264738866a0239993ca59d0a7b30c59e1c7
Mathis <mathisg@wp-corp.eu.org>
2023-11-08 16:32:42 +0100

    Removed useless logging

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import threading
 from get_text import record_text
 from cursor import print_cursor
 from video.src.emotion_webcam_demo import record_video
+from transformers import logging
 
 
 def main():
+    logging.set_verbosity_error()
     thread_text = threading.Thread(target = record_text)  
     thread_video = threading.Thread(target = record_video)
     thread_cursor = threading.Thread(target = print_cursor)
