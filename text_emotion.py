cbac1b66b556b8b9a2a2ad2e74327329c4523fc7
Gallier Loric <loric.gallier@etu.univ-grenoble-alpes.fr>
2023-11-14 20:15:23 +0100

    change emotion

--- text_emotion.py
+++ text_emotion.py
@@ -9,14 +9,23 @@ def get_emotion(text):
     result = classifier(text)
     return result[0]['label']
 
+
+# Les prédictions possibles sont : "colère", "joie", "peur", "s tristesse", "surprise"
+#   0 : colère
+#   1 : joie
+#   2 : peur
+#   3 : tristesse
+#   4 : surprise
+
 def get_emotionV2(text):
     model = FlaubertForSequenceClassification.from_pretrained("flaubert/flaubert_small_cased")
     tokenizer = AutoTokenizer.from_pretrained("flaubert/flaubert_small_cased")
     input_ids = tokenizer(text, return_tensors="pt").input_ids
     outputs = model(input_ids)
+    print (outputs)
     predictions = outputs.logits.argmax(dim=1)
+    print (predictions)
 
-    # Les prédictions possibles sont : "colère", "joie", "peur", "s tristesse", "surprise"
     émotion_dominante = predictions.item()
     print(f"L'émotion dominante dans votre texte est : {émotion_dominante}")
     return émotion_dominante
